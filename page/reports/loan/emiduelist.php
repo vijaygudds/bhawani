3ddbad0a4af5e4aea8db5f3fbf456edf12079b2e
IT <vijay.mali552@gmail.com>
2022-12-26 14:10:50 +0530

    remove checking field in grid list

--- page/reports/loan/emiduelist.php
+++ page/reports/loan/emiduelist.php
@@ -88,7 +88,7 @@ class page_reports_loan_emiduelist extends Page {
 		// $account_model_j->addField('DueDate');
 		// $account_model->addCondition('MaturedStatus',false); //???
 
-		$grid_column_array = array('AccountNumber','is_bike_returned','is_given_for_legal_process','bike_surrendered','created_at','maturity_date','last_transaction_date','Amount','due_date','scheme','current_balance','member_name','FatherName','CurrentAddress','landmark','tehsil','district','city','pin_code','state','PhoneNos','dealer','guarantor_name','guarantor_phno','guarantor_father','guarantor_address','last_premium','paid_premium_count','due_premium_count','emi_amount','emi_dueamount','due_panelty','received_panelty','remaning_panelty','other_charges','other_received','remaning_other_amount','gst_amount_dr','gst_amount_cr','gst_due','total','bike_surrendered_by','bike_surrendered_on','bike_returned_on','last_premium_date','interest_rate');
+		$grid_column_array = array('AccountNumber','created_at','maturity_date','last_transaction_date','Amount','due_date','scheme','current_balance','member_name','FatherName','CurrentAddress','landmark','tehsil','district','city','pin_code','state','PhoneNos','dealer','guarantor_name','guarantor_phno','guarantor_father','guarantor_address','last_premium','paid_premium_count','due_premium_count','emi_amount','emi_dueamount','due_panelty','received_panelty','remaning_panelty','other_charges','other_received','remaning_other_amount','gst_amount_dr','gst_amount_cr','gst_due','total','bike_surrendered_by','bike_surrendered_on','bike_returned_on','last_premium_date','interest_rate');
 
 		// $account_model->addExpression('loan_remark',function($m,$q){
 
