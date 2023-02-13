171e587f7065292099794a2fedee1b6fb8a235d4
IT <vijay.mali552@gmail.com>
2023-02-13 18:04:23 +0530

    uncommet loan calculation code

--- lib/Grid/Report/MemberDepositeAndLoan.php
+++ lib/Grid/Report/MemberDepositeAndLoan.php
@@ -49,34 +49,34 @@ class Grid_Report_MemberDepositeAndLoan extends Grid_AccountsBase{
 
 	function formatRow(){
 		//Code
-		// $account_model	= $this->add('Model_Account');
-		// $account_model->addCondition('member_id',$this->model->id);
-		// $account_model->addCondition('SchemeType','Loan');
-		// $cr = 0;
-		// $dr = 0;
-		// $purpose_for_loan_array = array();
-		// foreach ($account_model as $account) {
-		// 	$array = $account->getOpeningBalance($this->api->nextDate($this->as_on_date));
-		// 	$cr += $array['CR'];
-		// 	$dr += $array['DR'];
+		$account_model	= $this->add('Model_Account');
+		$account_model->addCondition('member_id',$this->model->id);
+		$account_model->addCondition('SchemeType','Loan');
+		$cr = 0;
+		$dr = 0;
+		$purpose_for_loan_array = array();
+		foreach ($account_model as $account) {
+			$array = $account->getOpeningBalance($this->api->nextDate($this->as_on_date));
+			$cr += $array['CR'];
+			$dr += $array['DR'];
 
-		// 	if(!in_array($account['account_type'], $purpose_for_loan_array))
-		// 		$purpose_for_loan_array[] = $account['account_type'];
-		// }	
+			if(!in_array($account['account_type'], $purpose_for_loan_array))
+				$purpose_for_loan_array[] = $account['account_type'];
+		}	
 
-		// $amount = $cr-$dr;
-		// $balance = $amount.' CR';
-		// if($amount < 0)
-		// 	$balance = abs($amount).' DR';
+		$amount = $cr-$dr;
+		$balance = $amount.' CR';
+		if($amount < 0)
+			$balance = abs($amount).' DR';
 
-		// //Loan Amount 
-		// $this->current_row['loan_amount'] = $balance;
+		//Loan Amount 
+		$this->current_row['loan_amount'] = $balance;
 		
-		// $purpose_for_loan=""; 
-		// foreach ($purpose_for_loan_array as $key => $value) {
-		// 	$purpose_for_loan .='<br>'.$value;
-		// }
-		// $this->current_row_html['purpose_for_loan'] = $purpose_for_loan;
+		$purpose_for_loan=""; 
+		foreach ($purpose_for_loan_array as $key => $value) {
+			$purpose_for_loan .='<br>'.$value;
+		}
+		$this->current_row_html['purpose_for_loan'] = $purpose_for_loan;
 
 
 		
